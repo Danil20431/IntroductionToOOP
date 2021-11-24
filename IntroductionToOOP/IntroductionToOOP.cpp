cae9e70200faa0e12e002816d6f7a8c89327d3d7
Vali_oh42@itstepru.onmicrosoft.com <Vali_oh42@itstepru.onmicrosoft.com>
2021-11-24 20:20:25 +0300

    Третье занятие. Часть 1

--- IntroductionToOOP/IntroductionToOOP.cpp
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -28,9 +28,59 @@ public:
     {
         this->y = y;
     }
+
+    //         Constructors:
+    /*Point()
+    {
+        x = y = 0;
+        cout << "DefoultConstructor:\t" << this << endl;
+    }*/
+    //Point(double x)
+    //{
+    //    this->x = x;
+    //    this->y = 0;  // double() - значение по умолчанию для типа double
+    //    cout << "1argConstructor:\t" << this << endl;
+    //}
+    Point(double x=0, double y=0)
+    {
+        this->x = x;
+        this->y = y;
+        cout << "Constructor:\t" << this << endl;
+    }
+    ~Point()
+    {
+        cout<< "Destructor:\t" << this << endl;
+    }
+
+    // Methods
+    double distance(Point other)
+    {
+        //other - другой (другая точка)
+        double x_distance = this->x - other.x;
+        double y_distance = this->y - other.y;
+        double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+        //sqrt - Square Root (квадратный корень)
+        return distance;
+    }
+
+    void print()const
+    {
+        cout << "X = " << x << "\tY = " << y << endl;
+    }
 };
 
 //#define STRUCT_POINT
+//#define CONSTRUCTORS_CHECK
+
+double distance(Point A, Point B)
+{
+    double x_distance = A.get_x() - B.get_x();
+    double y_distance = A.get_y() - B.get_y();
+    return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+
+//Point G - глобальный объект.
+int g; //Глобальная переменная. Осуждается
 
 int main()
 {
@@ -52,10 +102,30 @@ int main()
 
 #endif STRUCT_POINT
 
+#ifdef CONSTRUCTORS_CHECK
     Point A;
-    A.set_x(2);
-    A.set_y(3);
-    cout << A.get_x() << "\t" << A.get_y() << endl;
+    /*A.set_x(2);
+    A.set_y(3);*/
+    //cout << A.get_x() << "\t" << A.get_y() << endl;
+    A.print();
 
+    Point B(4, 5);
+    B.print();
+
+    Point C = 5; //single-argument constructor
+    C.print();
+
+    Point D(8);
+    D.print();
+
+#endif // CONSTRUCTORS_CHECK
+
+    Point A(2, 3);
+    Point B(3, 4);
+    cout << "Расстояние от точки A до B: " << A.distance(B) << endl;
+    cout << "Расстояние от точки A до B: " << B.distance(A) << endl;
+
+    cout << "Расстояние между точками A и B: " << distance(A, B) << endl;
+    cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
     return 0;
 }
